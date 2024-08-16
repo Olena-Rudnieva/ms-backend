72ffdd34e7ef237e3109747cfa01910d1e12d9c7
Olena-Rudnieva <rudnievao@gmail.com>
2024-08-16 13:09:52 +0300

    Common fixes

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 import mongoose from 'mongoose';
 import app from './app.js';
 
-//XrWaqlQUnSonKb8S
 const { DB_HOST, PORT = 3002 } = process.env;
-// const DB_HOST =
-//   'mongodb+srv://Olena:XrWaqlQUnSonKb8S@cluster0.4n3ip.mongodb.net/contacts-form?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose
   .connect(DB_HOST)
